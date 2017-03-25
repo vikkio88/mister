6dd43d5eea52efe3680ffc3f68b6c26a9b330e42
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-25 22:36:52 +0000

    imported and fixed random filler

--- src/main/java/com/mister/lib/helpers/RandomFiller.java
+++ src/main/java/com/mister/lib/helpers/RandomFiller.java
@@ -3,13 +3,25 @@ package com.mister.lib.helpers;
 import java.util.Random;
 
 public class RandomFiller {
-    Random random;
+    static Random random = new Random();
 
-    public RandomFiller() {
-        this.random = new Random();
+    public static int rand(int high) {
+        return random.nextInt(high);
     }
 
-    public int rand(int high) {
-        return random.nextInt(high);
+    public static int rand(int low, int high) {
+        return rand((high - low) + 1) + low;
+    }
+
+    public static int finance() {
+        if (chance(20)) {
+            return rand(10, 50);
+        }
+
+        return rand(1, 20);
+    }
+
+    public static boolean chance(int percentage) {
+        return rand(100) < percentage;
     }
 }
--- src/test/java/tests/com/mister/lib/helpers/RandomFillerTest.java
+++ src/test/java/tests/com/mister/lib/helpers/RandomFillerTest.java
@@ -6,19 +6,31 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class RandomFillerTest {
-    private RandomFiller randomFiller;
-
-    @Before
-    public void setUp() {
-        randomFiller = new RandomFiller();
+    @Test
+    public void itReturnsARandomNumberWithinALimit() {
+        int times = 20;
+        int high = 100;
+        for (int i = 0; i < times; i++) {
+            Assert.assertTrue(RandomFiller.rand(high) < high);
+        }
     }
 
     @Test
     public void itReturnsARandomNumberWithinARange() {
         int times = 20;
         int high = 100;
+        int low = 80;
         for (int i = 0; i < times; i++) {
-            Assert.assertTrue(randomFiller.rand(high) < high);
+            Assert.assertTrue(RandomFiller.rand(low, high) <= high);
+            Assert.assertTrue(RandomFiller.rand(low, high) >= low);
+        }
+    }
+
+    @Test
+    public void chanceReturnsValidOutput() {
+        int times = 100;
+        for (int i = 0; i < times; i++) {
+            Assert.assertTrue(RandomFiller.chance(100));
         }
     }
 }
