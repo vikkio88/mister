88ef029e367a0ca75262e54c6101cab53d7f1496
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-25 22:44:24 +0000

    scaffolding new Controls on NewGame scene

--- src/main/java/com/mister/app/controllers/NewGameController.java
+++ src/main/java/com/mister/app/controllers/NewGameController.java
@@ -1,5 +1,26 @@
 package com.mister.app.controllers;
 
+import com.mister.lib.model.Team;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+
+import java.awt.*;
+
 public class NewGameController extends BaseAppController {
+    @FXML
+    TextField name;
+    @FXML
+    TextField age;
+    @FXML
+    ComboBox<String> nationality;
+    @FXML
+    TableView<Team> teamsTable;
+
+    @Override
+    public void initialize(){
+
+    }
+
 
 }
