1f18527253592990b87d1369f6ec47df701a4725
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-25 14:21:23 +0000

    removed useless import

--- src/main/java/com/mister/app/Main.java
+++ src/main/java/com/mister/app/Main.java
@@ -2,12 +2,8 @@ package com.mister.app;
 
 import com.fxhelper.loader.SceneLoader;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class Main extends Application {
 
