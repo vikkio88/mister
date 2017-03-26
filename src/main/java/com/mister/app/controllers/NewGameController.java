e08acf838558dd1bfe20a140118ebab04cb5e60a
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-26 20:04:45 +0100

    replaced long methods with lambdas added binding on selection

--- src/main/java/com/mister/app/controllers/NewGameController.java
+++ src/main/java/com/mister/app/controllers/NewGameController.java
@@ -4,6 +4,10 @@ import com.mister.app.helper.Context;
 import com.mister.lib.helpers.RandomFiller;
 import com.mister.lib.model.Team;
 import com.mister.lib.model.enums.Nationality;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.BooleanExpression;
+import javafx.beans.binding.StringBinding;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
@@ -41,51 +45,42 @@ public class NewGameController extends BaseAppController {
     @FXML
     private TextField selectedTeamName;
 
-    Team selected = null;
+    private Team selected = null;
 
     @Override
     public void initialize() {
+
+        detailsButton.disableProperty().bind(new BooleanBinding() {
+            @Override
+            protected boolean computeValue() {
+                return selected != null;
+            }
+        });
+        nextButton.disableProperty().bind(new BooleanBinding() {
+            @Override
+            protected boolean computeValue() {
+                return selected != null;
+            }
+        });
+
         ArrayList<Team> teams = RandomFiller.getTeams(18);
         Context.getInstance().teamList = FXCollections.observableList(teams);
         Nationality[] nationalities = Nationality.values();
         Context.getInstance().nationalities = FXCollections.observableArrayList(nationalities);
         nationality.setItems(Context.getInstance().nationalities);
 
-        teamName.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Team, String>, ObservableValue<String>>() {
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Team, String> p) {
-                return new SimpleStringProperty(p.getValue().getName());
-            }
-        });
-        playersNumber.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Team, String>, ObservableValue<String>>() {
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Team, String> p) {
-                return new SimpleStringProperty(Integer.toString(p.getValue().getRoster().size()));
-            }
-        });
-        avgSkill.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Team, Float>, ObservableValue<Float>>() {
-            public ObservableValue<Float> call(TableColumn.CellDataFeatures<Team, Float> p) {
-                return new SimpleObjectProperty<Float>(Float.parseFloat("1.0"));
-            }
-        });
-        finances.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Team, Integer>, ObservableValue<Integer>>() {
-            public ObservableValue<Integer> call(TableColumn.CellDataFeatures<Team, Integer> p) {
-                return new SimpleObjectProperty<Integer>(p.getValue().getFinance());
-            }
-        });
-
+        teamName.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getName()));
+        playersNumber.setCellValueFactory(p -> new SimpleStringProperty(Integer.toString(p.getValue().getRoster().size())));
+        avgSkill.setCellValueFactory(p -> new SimpleObjectProperty<>(Float.parseFloat("1.0")));
+        finances.setCellValueFactory(p -> new SimpleObjectProperty<>(p.getValue().getFinance()));
         teamsTable.setItems(Context.getInstance().teamList);
-    }
-
-    public void clickOnTable(MouseEvent event) {
-        Node node = ((Node) event.getTarget()).getParent();
-        TableRow row;
-        if (node instanceof TableRow) {
-            row = (TableRow) node;
-            selected = (Team) row.getItem();
-            selectedTeamName.setText(selected.getName());
-            nextButton.setDisable(false);
-            detailsButton.setDisable(false);
-        }
-
+        teamsTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) ->
+        {
+            if (newSelection != null) {
+                selected = newSelection;
+                selectedTeamName.setText(selected.getName());
+            }
+        });
     }
 
     public void next(ActionEvent actionEvent) {
