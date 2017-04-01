ee0cc4f6d3e89b707345d2f31533efe1f0e82ff9
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-04-01 20:43:31 +0100

    linking first properties to main game view

--- src/main/java/com/mister/app/controllers/GameMainController.java
+++ src/main/java/com/mister/app/controllers/GameMainController.java
@@ -1,17 +1,39 @@
 package com.mister.app.controllers;
 
+import com.mister.app.game.model.HumanPlayer;
+import com.mister.app.game.model.PlayerTeam;
 import com.mister.app.helper.Context;
+import com.mister.lib.model.Player;
 import com.mister.lib.model.Team;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 
 public class GameMainController extends BaseAppController {
     @FXML
     public Label teamName;
+    @FXML
+    public Label playerName;
+    @FXML
+    public Label messageLabel;
+    @FXML
+    public ProgressBar trustBar;
+    @FXML
+    public ProgressBar moraleBar;
 
     @Override
     public void initialize() {
-        Team playerTeam = Context.getInstance().selectedTeam;
-        teamName.setText(playerTeam.getName());
+        PlayerTeam playerTeam = Context.getInstance().humanTeam;
+        HumanPlayer player = Context.getInstance().humanPlayer;
+        teamName.setText(playerTeam.getTeam().getName());
+        trustBar.progressProperty().bind(new SimpleIntegerProperty(playerTeam.getTrust()));
+        moraleBar.progressProperty().bind(new SimpleIntegerProperty(playerTeam.getMorale()));
+        playerName.setText(player.getFullName());
+    }
+
+    public void advance(ActionEvent actionEvent) {
+        System.out.println("Simulate");
     }
 }
