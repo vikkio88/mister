01fc308b8314d1e1e2a75d71f7c823e0c401601b
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-04-03 21:16:01 +0100

    team names on Match result

--- src/main/java/com/mister/lib/model/MatchResult.java
+++ src/main/java/com/mister/lib/model/MatchResult.java
@@ -5,12 +5,16 @@ import java.util.List;
 public class MatchResult {
     private final int goalHome;
     private final int goalAway;
+    private final String homeTeam;
+    private final String awayTeam;
     private final List<Player> scorersHome;
     private final List<Player> scorersAway;
 
-    public MatchResult(int goalHome, int goalAway, List<Player> scorersHome, List<Player> scorersAway) {
+    public MatchResult(int goalHome, int goalAway, String homeTeam, String awayTeam, List<Player> scorersHome, List<Player> scorersAway) {
         this.goalHome = goalHome;
         this.goalAway = goalAway;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
         this.scorersHome = scorersHome;
         this.scorersAway = scorersAway;
     }
@@ -21,5 +25,18 @@ public class MatchResult {
 
     public int getGoalAway() {
         return goalAway;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d - %d %s", homeTeam, goalHome, goalAway, awayTeam);
+    }
+
+    public List<Player> getScorersHome() {
+        return scorersHome;
+    }
+
+    public List<Player> getScorersAway() {
+        return scorersAway;
     }
 }
